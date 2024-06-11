a9b96f61016690019aa8769110054e8f0e1972d1
Bruno Vieira Correa <bruno345viera@gmail.com>
2024-06-11 11:28:56 -0300

    return data login

--- FrontEnd/src/services/api/auth/AuthService.tsx
+++ FrontEnd/src/services/api/auth/AuthService.tsx
@@ -13,6 +13,12 @@ export const auth = async (cpf: string, senha: string) => {
     });
     console.log(data);
 
+    if (data.data == null) {
+      console.log("nenhum usuario encontrado");
+      return data;
+    } else {
+      return 1;
+    }
     return data;
   } catch (error) {
     console.error(error);
